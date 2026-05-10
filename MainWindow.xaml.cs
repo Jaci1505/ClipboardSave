ec0412e7a5246987f7c105d44d3f58bf3f927a9c
jschroe9 <jschroe9@hs-mittweida.de>
2026-05-10 10:26:04 +0200

    Vollständige Dokumentation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,30 +20,34 @@ using System.Windows.Shapes;
 namespace ClipboardSave
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaktionslogik des Fensters für die Hauptfensters.
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Konstruktor zur Initialisierung des Hauptfensters. 
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
-            System.Windows.Media.Imaging.BitmapSource image = Clipboard.GetImage();
+            System.Windows.Media.Imaging.BitmapSource image = Clipboard.GetImage(); //aktuelles Bild aus dem Clipboard holen
 
-            if (image == null)
+            if (image == null) //kein Bild im Clipboard vorhanden
             {
                 showErrorBoxAndClose("Fehler! Es wurde kein Screenshot in der Zwischenablage gefunden. Das Programm wird beendet");
             }
 
             else
             {
-                string path = showSaveFileDialog();
+                string path = showSaveFileDialog(); //Speicherdialog anzeigen
                 try
                 {
+                    //Bild speichern
                     using (var fileStream = new FileStream(path, FileMode.Create))
                     {
                         BitmapEncoder encoder = new PngBitmapEncoder();
                         encoder.Frames.Add(BitmapFrame.Create(image));
-                        encoder.Save(fileStream);
+                        encoder.Save(fileStream); 
                     }
 
                 }
@@ -57,16 +61,19 @@ namespace ClipboardSave
                 }
             }
 
-            Application.Current.Shutdown();
+            Application.Current.Shutdown(); //Nach Speichern des Bildes Applikation beenden
 
         }
 
 
-
+        /// <summary>
+        /// Zeige einen Dialog zum Speichern der Bilddatei. 
+        /// </summary>
+        /// <returns>Dateinamen den Nutzer gewählt hat</returns>
         public static string showSaveFileDialog()
         {
             SaveFileDialog sfd = new SaveFileDialog();
-            sfd.Filter = "PNG-Bild(*png)|*.png|JPG-Bild(*jpg)|*.jpg"; //nur Excel-Dateien zulassen
+            sfd.Filter = "PNG-Bild(*png)|*.png|JPG-Bild(*jpg)|*.jpg"; //nur jpg und png-Dateien zulassen
             sfd.InitialDirectory = "C:\\"; //starte im Verzeichnis C
             bool? result = sfd.ShowDialog();
             if (result == true) //etwas gespeichert (Pfad gewaehlt)
@@ -76,18 +83,32 @@ namespace ClipboardSave
             else return null; //Dialog geschlossen oder abgebrochen  
         }
 
+        /// <summary>
+        /// Zeigt eine ein Dialogfenster für einen Fehler. Dieser besitzt den Knopf "OK" zur Bestätigung. Danach wird die Applikation sofort beendet.
+        /// </summary>
+        /// <param name="errorText">Text der Fehlermeldung</param>
         public static void showErrorBoxAndClose(string errorText)
         {
             MessageBox.Show(errorText, "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
             Application.Current.Shutdown(); //beende Programm
         }
-
+        
+        /// <summary>
+        /// Zeigt eine Fehlermeldung, dass eine Datei nicht unter einem bestimmten Namen gespeichert werden kann, da sie bereits offen ist.
+        /// </summary>
+        /// <param name="window">Fenster, aus dem Dialog aufgerufen wurde</param>
         public static void showCannotBeSaveUnderNamedError(Window window)
         {
             showErrorBox("Fehler! Die Datei kann nicht unter diesem Namen gespeichert werden, da eine Datei unter diesem Namen bereits offen ist!", window);
 
 
         }
+
+        /// <summary>
+        /// Zeigt eine ein Dialogfenster für einen Fehler. Dieser besitzt die Knöpfe "OK" zur Bestätigung und Weiterarbeit und "Beenden" um die Applikation zu schließen.
+        /// </summary>
+        /// <param name="errorText">Text der Fehlermeldung</param>
+        /// <param name="window">Fenster, aus dem Dialog aufgerufen wurde</param>
         public static void showErrorBox(string errorText, Window window)
         {
             TaskDialog td = new TaskDialog();
